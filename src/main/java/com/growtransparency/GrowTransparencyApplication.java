61e7a92651e5ae33a0fb6389450f6eddd6e7cfb2
LucasAuSilva <silvaaugustolucas@gmail.com>
2022-11-03 13:19:33 -0300

    feat: change some configs for swagger in controllers

--- src/main/java/com/growtransparency/GrowTransparencyApplication.java
+++ src/main/java/com/growtransparency/GrowTransparencyApplication.java
@@ -1,9 +1,15 @@
 package com.growtransparency;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Grow Transparency API", version = "1.0", description = "Grow Transparency API"))
+@SecurityScheme(name = "bearerAuth", type = SecuritySchemeType.HTTP, scheme = "bearer", bearerFormat = "JWT")
 public class GrowTransparencyApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GrowTransparencyApplication.class, args);
--- src/main/java/com/growtransparency/controllers/UserController.java
+++ src/main/java/com/growtransparency/controllers/UserController.java
@@ -8,12 +8,22 @@ import com.growtransparency.models.User;
 import com.growtransparency.repositories.RoleRepository;
 import com.growtransparency.repositories.UserRepository;
 import com.growtransparency.services.TokenService;
+import com.growtransparency.settings.errors.ResponseErrorValidation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityRequirements;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import javax.validation.Valid;
+import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequestMapping("/user")
@@ -32,12 +42,20 @@ public class UserController {
   }
 
   @PostMapping("/register")
-  public ResponseEntity<ReturnCreatedUserDTO> registerUser(@Valid @RequestBody CreateUserDTO dto) {
+  @ApiResponses(value = {
+    @ApiResponse(responseCode = "201", description = "User created"),
+    @ApiResponse(responseCode = "400", description = "Invalid request")})
+  public ResponseEntity<ReturnCreatedUserDTO> registerUser(@Valid @RequestBody CreateUserDTO dto, UriComponentsBuilder uriBuilder) {
     User user = userRepository.save(dto.toEntity());
-    return ResponseEntity.ok(new ReturnCreatedUserDTO(user));
+    URI uri = uriBuilder.path("/user/{id}").buildAndExpand(user.getId()).toUri();
+    return ResponseEntity.created(uri).body(new ReturnCreatedUserDTO(user));
   }
 
   @PostMapping("/login")
+  @ApiResponses(value = {
+    @ApiResponse(responseCode = "200", description = "Login realizado com sucesso"),
+    @ApiResponse(responseCode = "401", description = "Não autorizado", content = @Content),
+  })
   public ResponseEntity<ReturnLoginUserDto> loginUser(@RequestBody LoginUserDTO dto) {
     var optional = userRepository.findByEmail(dto.getEmail());
 
@@ -49,10 +67,19 @@ public class UserController {
       return ResponseEntity.ok(new ReturnLoginUserDto(token, true));
     }
 
-    return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(new ReturnLoginUserDto("", false));
+    return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
   }
 
   @PutMapping("/admin/{id}")
+  @SecurityRequirements(value = {
+    @SecurityRequirement(name = "authBearer")
+  })
+  @ApiResponses(value = {
+    @ApiResponse(responseCode = "204", description = "User updated", content = @Content),
+    @ApiResponse(responseCode = "418", description = "User already admin", content = @Content),
+    @ApiResponse(responseCode = "403", description = "User authenticated but not admin", content = @Content),
+    @ApiResponse(responseCode = "404", description = "User not found", content = @Content),
+  })
   public ResponseEntity<?> nominateAdmin(@PathVariable Long id) {
     var optional = userRepository.findById(id);
     var rolesOptional = roleRepository.findById(1L);
