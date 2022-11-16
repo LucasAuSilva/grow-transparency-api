6b15aeb3c509f35f5f258e5842857b6f061bc5b2
LucasAuSilva <silvaaugustolucas@gmail.com>
2022-11-16 18:11:14 -0300

    feat: add validation to create project

--- src/main/java/com/growtransparency/dtos/CreateProjectDTO.java
+++ src/main/java/com/growtransparency/dtos/CreateProjectDTO.java
@@ -2,15 +2,18 @@ package com.growtransparency.dtos;
 
 import com.growtransparency.models.Project;
 
+import javax.validation.constraints.NegativeOrZero;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 
 public record CreateProjectDTO(
-        @NotBlank String name,
+        @NotBlank(message = "Nome é um campo obrigatório") String name,
         String description,
-        Double cost,
-        Integer totalTime,
+        @NotNull(message = "Custo é um campo obrigatório") Double cost,
+        @NotNull(message = "Tempo total é um campo obrigatório") Integer totalTime,
         Integer status,
-        Double score,
+        @NotNull(message = "Pontuação é um campo obrigatório") Double score,
         String link) {
   public Project toEntity() {
     return new Project(name, description, cost, totalTime, status, score, link);
