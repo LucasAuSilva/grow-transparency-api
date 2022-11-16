0c77158ad6db7a9a68aab274c8c42f7fc48ab016
LucasAuSilva <silvaaugustolucas@gmail.com>
2022-11-16 16:38:50 -0300

    refactor: add roles on login return

--- src/main/java/com/growtransparency/dtos/ReturnLoginUserDto.java
+++ src/main/java/com/growtransparency/dtos/ReturnLoginUserDto.java
@@ -1,12 +1,16 @@
 package com.growtransparency.dtos;
 
+import java.util.List;
+
 public class ReturnLoginUserDto {
   private String token;
   private boolean result;
+  private List<String> roles;
 
-  public ReturnLoginUserDto(String token, boolean result) {
+  public ReturnLoginUserDto(String token, boolean result, List<String> roles) {
     this.token = token;
     this.result = result;
+    this.roles = roles;
   }
 
   public String getToken() {
@@ -24,4 +28,8 @@ public class ReturnLoginUserDto {
   public void setResult(boolean result) {
     this.result = result;
   }
+
+  public List<String> getRoles() { return roles; }
+
+  public void setRoles(List<String> roles) { this.roles = roles; }
 }
