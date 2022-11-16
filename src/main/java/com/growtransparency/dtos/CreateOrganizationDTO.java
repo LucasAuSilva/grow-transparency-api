0743966436fdbaa511e8fc06d50565bfcbe8d313
LucasAuSilva <silvaaugustolucas@gmail.com>
2022-11-16 18:15:35 -0300

    feat: add validation to create organization

--- src/main/java/com/growtransparency/dtos/CreateOrganizationDTO.java
+++ src/main/java/com/growtransparency/dtos/CreateOrganizationDTO.java
@@ -4,7 +4,8 @@ import com.growtransparency.models.Organization;
 
 import javax.validation.constraints.NotBlank;
 
-public record CreateOrganizationDTO(@NotBlank String name) {
+public record CreateOrganizationDTO(
+        @NotBlank(message = "Nome é um campo obrigatório") String name) {
   public Organization toEntity() {
     return new Organization(name);
   }
